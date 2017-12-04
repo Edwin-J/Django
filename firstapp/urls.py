503def35b5e9f76c59d53ce54b1267ae33f30ea4
Edwin-J <gkdhaosl@naver.com>
2017-12-04 12:08:52 +0900

    Made new function

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -2,9 +2,13 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+
     url(r'^main', views.main, name='main'),
     url(r'^introduce', views.introduce, name='introduce'),
     url(r'^study', views.study, name='study'),
-    url(r'^post/new/$', views.post_new, name='post_new'),
+    
+    url(r'^base', views.base, name='base'),
+    url(r'^post/(?P<post_id>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^post/new', views.post_new, name='post_new'),
+    url(r'^$', views.post_list, name='post_list'),
     ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Post
+from django.utils import timezone
 from .forms import PostForm
+from django.contrib.auth.models import User
 
-
+user = User.objects.get(username='admin')
 def post_list(request):
     posts=Post.objects.all()
     return render(request, 'firstapp/home.html',{'posts':posts})
@@ -19,20 +21,36 @@ def introduce(request) :
 def study(request) :
     return render(request, 'firstapp/study.html')
 
-def post_new(request) :
-    form = postForm()
-    return render(request, 'firstapp/post_edit.html', {'form' : form})
+
+def base(request) :
+    return render(request, 'firstapp/base.html')
+
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = request.user
+            post.author = user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect('../../', pk=post.pk)
     else:
         form = PostForm()
+        
     return render(request, 'firstapp/post_edit.html', {'form': form})
+    
 
-# Create your views here.
+def post_edit(request, post_id) :
+    post = get_object_or_404(Post, pk = post_id)
+    if request.method == "POST" :
+        form = PostForm(request.POST)
+        if form.is_valid() :
+            post = form.save(commit = False)
+            post.author = user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('firstapp.views.post_detail', post_id = post.pk)
+    else :
+        form = PostForm(instance = post)
+
+    return render(request, 'firstapp/post_edit.html', {'form' : form})
